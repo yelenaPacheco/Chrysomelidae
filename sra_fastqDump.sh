993bed014d584e576042c0c7cb9268693de6a1ec
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2023-12-06 19:59:21 -0500

    Update sra_fastqDump.sh

--- sra_fastqDump.sh
+++ sra_fastqDump.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J sra_prefectch_dump
-#SBATCH -o sra_err/sra_prefectch_dump_%A_%a.out
-#SBATCH -e sra_err/sra_prefectch_dump_%A_%a.err
-#SBATCH --array=1-5
-#SBATCH -n 6 -N 1
+#SBATCH -o sra_prefectch_dump_%A_%a.out
+#SBATCH -e sra_prefectch_dump_%A_%a.err
+#SBATCH --array=1-105
+#SBATCH -n 12 -N 1
 #SBATCH -p short
-#SBATCH -t 2-00:00:00
+#SBATCH -t 24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yelena.marlese@gmail.com
 
@@ -17,9 +17,9 @@
 
 module load sratoolkit/3.0.2
 
-line=$(sed -n "$SLURM_ARRAY_TASK_ID"p srr_list.txt)
-sra_id=`echo "${line}"|cut -f1`
-taxon=`echo "${line}"|cut -f2`
+line=$(sed -n "$SLURM_ARRAY_TASK_ID"p Douglas_Chrysomelid.tsv)
+sra_id=`echo "${line}"|cut -f2`
+taxon=`echo "${line}"|cut -f1`
 echo $sra_id
 echo "$sra_id/${sra_id}.sra" 
 fastq-dump "$sra_id/${sra_id}.sra" --split-3 --skip-technical
