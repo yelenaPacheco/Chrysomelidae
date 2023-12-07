9bb2e125a8cf2b4d2cd1183f67943359fdb73cbf
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2023-12-06 19:59:23 -0500

    Update sraFetch.sh

--- sraFetch.sh
+++ sraFetch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -J sra_Fetch
-#SBATCH -o sra_err/sra_Fetch_%A_%a.out
-#SBATCH -e sra_err/sra_Fetch_%A_%a.err
-#SBATCH --array=1-5
-#SBATCH -n 40 -N 1
+#SBATCH -o sra_Fetch_%A_%a.out
+#SBATCH -e sra_Fetch_%A_%a.err
+#SBATCH --array=1-105
+#SBATCH -n 20 -N 1
 #SBATCH -p short
-#SBATCH -t 2-00:00:00
+#SBATCH -t 24:00:00
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yelena.marlese@gmail.com
 
@@ -18,8 +18,8 @@ module load sratoolkit/3.0.2
 
 
 ##sets up array
-##sra_fetch_dump_array.txt includes list of srx accession numbers and species names 
-line=$(sed -n "$SLURM_ARRAY_TASK_ID"p sra_fetch_dump_array.txt)
+#Douglas_Chrysomelid.csv includes list of srx accession numbers and species names 
+line=$(sed -n "$SLURM_ARRAY_TASK_ID"p Douglas_Chrysomelid.tsv)
 ##get just the species names (removes first field)
 ## cut is applied to individual line -f = field, 
 sra_id=`echo "${line}"|cut -f1`
