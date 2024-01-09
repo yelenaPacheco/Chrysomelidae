1692e089c81e6abc77ff2c0716d52e5a3ce7e7d9
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-01-09 16:17:05 -0500

    Create concatFasta.py

---
+++ concatFasta.py
@@ -0,0 +1,23 @@
+from Bio import SeqIO
+
+# List of input FASTA files
+input_files = ['file1.fasta', 'file2.fasta', 'file3.fasta']
+
+# Dictionary to store sequences for each taxon
+taxon_sequences = {}
+
+# Read sequences from each file and store them in the dictionary
+for file in input_files:
+    for record in SeqIO.parse(file, 'fasta'):
+        taxon = record.id.split('|')[0]  # Modify this line based on your header format
+        sequence = str(record.seq)
+        
+        if taxon not in taxon_sequences:
+            taxon_sequences[taxon] = sequence
+        else:
+            taxon_sequences[taxon] += sequence
+
+# Write combined sequences to a new FASTA file
+with open('combined.fasta', 'w') as output_file:
+    for taxon, sequence in taxon_sequences.items():
+        output_file.write(f'>{taxon}\n{sequence}\n')
