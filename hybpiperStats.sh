72816aa0f9490caaaeced35fdbff1603033e213a
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-01-30 12:11:36 -0500

    Create hybpiperStats.sh

---
+++ hybpiperStats.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J hybpiperStats   
+#SBATCH -o hybpiperStats_%A_%a.out
+#SBATCH -e hybpiperStats_%A_%a.err
+#SBATCH -n 20 -N 1
+#SBATCH -p short
+#SBATCH -t 2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yelena.marlese@gmail.com
+
+
+module load miniconda
+source activate /project/leafbeetle_phylogenomics/yelena/hybpiper
+
+
+hybpiper stats --targetfile_dna ../ColeopteraProbes.fasta   gene directory.list 
+
+
+conda deactivate
