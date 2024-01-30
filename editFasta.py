bc8cdad173038788b28a92d8c42d9634dec70126
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-01-30 12:12:40 -0500

    Update editFasta.py

--- editFasta.py
+++ editFasta.py
@@ -3,6 +3,11 @@ from Bio.Seq import Seq
 import glob
 import os
 
+#reformats fasta headers to work with Hybpiper
+#the locus number needs to be the last thing listed in the fasta header
+#original header format >Coleoptera-L1-Col1.8.56
+#new header format >Col1.8.56.Coleoptera-L1
+
 
 with open('Coleoptera_AHE_probes.tsv', 'r') as f:
 	with open ('ColeopteraProbes.fasta', 'w') as fasta:
