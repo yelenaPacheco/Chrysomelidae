5305e1794e2b6f7d35112f24978adc0691e4b2c8
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-01-30 13:26:18 -0500

    Update spadesGenome.sh

--- spadesGenome.sh
+++ spadesGenome.sh
@@ -2,6 +2,7 @@
 #SBATCH -J spades
 #SBATCH -o spades_%A_%a.out
 #SBATCH -e spades_%A_%a.err
+#SBATCH --array=1-4
 #SBATCH -n 40 -N 1
 #SBATCH -p short
 #SBATCH -t 2-00:00:00
@@ -9,9 +10,16 @@
 #SBATCH --mail-user=yelena.marlese@gmail.com
 
 
+#set up array
+#inputReads2.list includes a list of all R1 reads 
+infile=$(cat inputReads2.list | head -n ${SLURM_ARRAY_TASK_ID} | tail -n1)
+
+base=$(basename ${infile} _P_R1.fastq)
+
+
 module load spades/3.15.5
 
-spades.py -1 <reads1> -2 <reads2> \
- 	-o spades_test
+spades.py -1 ${base}_P_R1.fastq  -2 ${base}_P_R2.fastq \
+        -s ${base}_U.fastq -o ${base}2_spades -m 560
 
- 
+
