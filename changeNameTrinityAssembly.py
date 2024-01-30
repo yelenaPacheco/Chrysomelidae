90c8a64382cf9ecfcc7b5a91b7236f86a730baf4
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-01-30 12:12:34 -0500

    Update changeNameTrinityAssembly.py

--- changeNameTrinityAssembly.py
+++ changeNameTrinityAssembly.py
@@ -4,6 +4,9 @@ import glob
 import os
 
 
+#this script renames trinity assembles with the extension *.trinity.fasta to *_trinity.fasta
+#blast.list is a list of trinity assembly file names
+
 with open('blast.list') as f:
 	for line in f:
 		taxaname=line.split(".",1)[0].strip()
