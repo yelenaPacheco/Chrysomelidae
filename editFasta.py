cae969c638cbe37bd94ccdfaa32e297f2983a4e5
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-03-21 12:51:02 -0400

    Update editFasta.py

--- editFasta.py
+++ editFasta.py
@@ -12,12 +12,12 @@ import os
 with open('Coleoptera_AHE_probes.tsv', 'r') as f:
 	with open ('ColeopteraProbes.fasta', 'w') as fasta:
 		for line in f:
-			if line.startswith('>')==TRUE:
+			if line.startswith('>')==True:
 				locus1=line.split('-')[0].strip()
 				locus2=locus1[1:]
 				refTax1=line.split('-')[1].strip()
 				refTax2=refTax1.split(' ')[0].strip()
-				fasta.write(">"+refTax2+"-"+locus2+'n')
+				fasta.write(">"+refTax2+"-"+locus2+'\n')
 			else: 
 				fasta.write(line.strip()+'\n')
 				
