455133f9e7771f6e56db316390c501b1625c2c4d
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-03-21 12:51:18 -0400

    Create gfaConvert.sh

---
+++ gfaConvert.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J gfaConvert
+#SBATCH -o gfaConvert_%A_%a.out
+#SBATCH -e gfaConvert_%A_%a.err
+#SBATCH -n 20 -N 1
+#SBATCH -p scavenger
+#SBATCH -t 24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yelena.pacheco@usda.gov
+
+module load miniconda
+source activate /project/leafbeetle_phylogenomics/yelena/hybpiper
+
+gfastats -f  N_clydesmithi.asm.bp.p_ctg.gfa -o fa -j 20
+
+conda deactivate
