37e055e51e48b78b4dc1f22c5f895102a9c14e30
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-03-29 11:49:42 -0400

    Create canu.sh

---
+++ canu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -J canu
+#SBATCH -o canu_%A.out
+#SBATCH -e canu_%A.err
+#SBATCH -n 40 -N 1
+#SBATCH -p scavenger
+#SBATCH -t 2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yelena.pacheco@usda.gov
+
+
+module load canu/2.2
+
+canu -p N_clydesmithi_asm -d test \
+genomeSize=6m \
+-pacbio-hifi  m84100_230830_180417_s4.hifi_reads.fastq.gz
+
+
