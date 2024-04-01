8f343cfcdc75175d296f8eeb0f46d21ef29ec42c
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-04-01 08:00:46 -0400

    Create racon.sh

---
+++ racon.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -J racon
+#SBATCH -o racon_%A.out
+#SBATCH -e racon_%A.err
+#SBATCH -n 40 -N 1
+#SBATCH -p short
+#SBATCH -t 2-00:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=yelena.marlese@gmail.com
+
+
+
+#--- Starrt timer
+t1=$(date +"%s")
+
+
+module load racon/1.5.0
+
+racon -t 40 ../m84100_230830_180417_s4.hifi_reads.fastq.gz  <samFile> N_clydesmithi_asm.contigs.fasta
+
+
+
+#---Complete job
+t2=$(date +"%s")
+diff=$(($t2-$t1))
+echo "[---$SN---] ($(date)) $(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
+echo "[---$SN---] ($(date)) $SN COMPLETE."
