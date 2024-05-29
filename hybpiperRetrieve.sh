9e1847fe07ca5553c3277e71a4ce466eb26378da
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-05-29 16:37:10 -0400

    Update hybpiperRetrieve.sh

--- hybpiperRetrieve.sh
+++ hybpiperRetrieve.sh
@@ -6,22 +6,30 @@
 #SBATCH -p short
 #SBATCH -t 2-00:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=yelena.marlese@gmail.com
+#SBATCH --mail-user=yelena.pacheco@usda.gov
 
 
 #second step in Hybpiper pipline, gathers all contigs for each locus for all taxa in --sample_names file 
 #output is a fasta file for each locus
 
 
+#--- Starrt timer
+t1=$(date +"%s")
+
 module load miniconda
 source activate /project/leafbeetle_phylogenomics/yelena/hybpiper
 
 
 
-hybpiper retrieve_sequences -t_dna ColeopteraProbes.fasta dna --sample_names directory.list --fasta_dir Douglas_sequences
+hybpiper retrieve_sequences -t_dna refSequences.fasta dna --sample_names directory.list --fasta_dir loci_fasta_files
 
 conda deactivate
 
+#---Complete job
+t2=$(date +"%s")
+diff=$(($t2-$t1))
+echo "[---$SN---] ($(date)) $(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
+echo "[---$SN---] ($(date)) $SN COMPLETE."
 
 
 # -t_dna fasta w/reference sequences 
