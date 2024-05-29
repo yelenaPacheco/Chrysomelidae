97a3045f50f1026dcff18b057a80434c883c6135
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-05-29 16:37:13 -0400

    Update hybpiperStats.sh

--- hybpiperStats.sh
+++ hybpiperStats.sh
@@ -8,8 +8,12 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=yelena.pacheco@uga.edu
 
-#option step 3 in hyhbpiper pipeline
-#output gives a .tsv file with the number of reads mapped, number of contigs, etc. 
+#optional step 3 in hyhbpiper pipeline
+#output gives a .tsv file with the number of reads mapped, number of contigs, etc. per sample
+
+#--- Starrt timer
+t1=$(date +"%s")
+
 
 
 module load miniconda
@@ -20,6 +24,13 @@ hybpiper stats --targetfile_dna ../ColeopteraProbes.fasta   gene directory.list
 
 
 conda deactivate
+
+
+#---Complete job
+t2=$(date +"%s")
+diff=$(($t2-$t1))
+echo "[---$SN---] ($(date)) $(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
+echo "[---$SN---] ($(date)) $SN COMPLETE."
 
 
 # --targetfile_dna fasta file w/reference sequences
