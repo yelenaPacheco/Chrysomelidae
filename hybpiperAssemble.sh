d3c1b6c0e3ab13c8dfbd6861149cd1074c36e4d6
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-05-29 16:37:08 -0400

    Update hybpiperAssemble.sh

--- hybpiperAssemble.sh
+++ hybpiperAssemble.sh
@@ -7,17 +7,20 @@
 #SBATCH -p short
 #SBATCH -t 2-00:00:00
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=yelena.marlese@gmail.com
+#SBATCH --mail-user=yelena.pacheco@usda.gov
 
 #First step of hybpiper piperline, assembles individual loci contigs from raw reads for each individual taxon
 
+
+#--- Starrt timer
+t1=$(date +"%s")
 
 module load miniconda
 source activate /project/leafbeetle_phylogenomics/yelena/hybpiper
 
 
 
-infile=$(cat test1.list | head -n ${SLURM_ARRAY_TASK_ID} | tail -n1)
+infile=$(cat input2.list | head -n ${SLURM_ARRAY_TASK_ID} | tail -n1)
 
 base=$(basename ${infile} _P_R1.fastq)
 
@@ -25,13 +28,20 @@ base=$(basename ${infile} _P_R1.fastq)
 hybpiper assemble \
 -r ${base}_P_R1* ${base}_P_R2* \
 --unpaired ${base}_U* \
--t_dna ColeopteraProbes.fasta \
+-t_dna refSequences.fasta \
 --prefix ${base} \
 --bwa \
 --cpu 40 --bbmap_memory 10 --bbmap_threads 40
 
 
 conda deactivate
+
+
+#---Complete job
+t2=$(date +"%s")
+diff=$(($t2-$t1))
+echo "[---$SN---] ($(date)) $(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
+echo "[---$SN---] ($(date)) $SN COMPLETE."
 
 
 # test1.list list of al P_R1.fastq files
