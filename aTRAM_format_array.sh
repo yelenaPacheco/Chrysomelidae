e6f60d5b95d838fdac1d24e7692b4072c827b339
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-08-05 16:25:12 -0400

    Create aTRAM_format_array.sh

---
+++ aTRAM_format_array.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -J rename
+#SBATCH -o rename_%A_%a.out
+#SBATCH -e rename_%A_%a.err
+#SBATCH --array=1-3
+#SBATCH -n 10 -N 1
+#SBATCH -p atlas
+#SBATCH -t 2-00:00:00
+#SBATCH --mem=1G
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=yelena.pacheco@usda.gov
+
+
+#--- Starrt timer
+t1=$(date +"%s")
+
+
+infile=$(cat format1.list | head -n ${SLURM_ARRAY_TASK_ID} | tail -n1)
+
+
+ml miniconda3
+source activate /project/aphid_phylogenomics/yelena/software_y
+
+python aTRAM_formatFASTQ.py ${infile} new_${infile}
+
+
+conda deactivate 
+
+
+
+#---Complete job
+t2=$(date +"%s")
+diff=$(($t2-$t1))
+echo "[---$SN---] ($(date)) $(($diff / 60)) minutes and $(($diff % 60)) seconds elapsed."
+echo "[---$SN---] ($(date)) $SN COMPLETE."
