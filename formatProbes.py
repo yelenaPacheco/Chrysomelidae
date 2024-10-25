7610458566a00754d08bbf72e3305fe9f967dd95
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-10-25 13:44:29 -0400

    Create formatProbes.py

---
+++ formatProbes.py
@@ -0,0 +1,25 @@
+from Bio import SeqIO
+from Bio.Seq import Seq
+import glob
+import os
+
+#reformats probe fasta headers to work with Phyluce
+
+
+
+with open('Coleoptera-UCE-1.1K-v1_test.fasta', 'r') as f:
+	with open ('phyluceProbesFormat.fasta', 'w') as fasta:
+		for line in f:
+			if line.startswith('>')==True:
+				startText=line.split(' ')[0].strip()
+				source1=line.split(":",3)[2].strip()
+				source2=source1.split(',')[0].strip()
+				locus1=line.split('probeslocus:')[1].strip()
+				locus2=locus1.split(',')[0].strip()
+				probe1=lines.split('probesprobe:'),[1].strip()
+				probe2=probe1.split(',')[0].strip()
+				fasta.write(startText+' |source:'+source2+',probes-loucs:'+locus2+',probes-probe:'+probe2='\n')
+			else: 
+				fasta.write(line.strip()+'\n')
+				
+
