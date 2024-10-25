98dea5e9888911b61e3167d6fefe916847966263
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-10-25 13:45:09 -0400

    Create removeTrees.sh

---
+++ removeTrees.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J removeTrees
+#SBATCH -n 20 -N 1
+#SBATCH -p short,scavenger
+#SBATCH -t 24:00:00
+#SBATCH -e removeTrees_%A.err
+#SBATCH -A aphid_phylogenomics
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=yelena.pacheco@usda.gov
+
+
+
+for f in $(cat removeTrees.list) ; do 
+  rm "$f"
+done
