a4c3f09e98fa4352826d834aad9502bdf8c59b62
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2024-10-25 13:44:38 -0400

    Update hybpiperRetrieve.sh

--- hybpiperRetrieve.sh
+++ hybpiperRetrieve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -J hybpiperRetrieve
-#SBATCH -o hybpiperRetrieve_%A_%a.out
-#SBATCH -e hybpiperRetrieve_%A_%a.err
+#SBATCH -o hybpiperRetrieve_%A.out
+#SBATCH -e hybpiperRetrieve_%A.err
 #SBATCH -n 20 -N 1
 #SBATCH -p short
 #SBATCH -t 2-00:00:00
