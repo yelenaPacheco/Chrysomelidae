f6c61b0f29dc71992ab1ff2555376c1be78b5388
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2025-01-14 16:11:55 -0500

    Create explodeFasta.sh

---
+++ explodeFasta.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -J ex_fasta
+#SBATCH -o ex_fasta_%A.out
+#SBATCH -e ex_fasta_%A.err
+#SBATCH -n 20 -N 1
+#SBATCH -p atlas
+#SBATCH -A aphid_phylogenomics
+#SBATCH -t 2:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=yelena.pacheco@usda.gov
+
+module load phyluce/1.7.3
+
+# explode the monolithic FASTA by taxon (you can also do by locus)
+phyluce_assembly_explode_get_fastas_file \
+    --input all-taxa-incomplete.fasta \
+    --output exploded-fastas \
+    --by-taxon
+
+# get summary stats on the FASTAS
+for i in exploded-fastas/*.fasta;
+do
+    phyluce_assembly_get_fasta_lengths --input $i --csv;
+done
