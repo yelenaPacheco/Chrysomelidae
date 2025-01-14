3beefbce435a45408f1beed2b4499d9bd4e83f26
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2025-01-14 16:01:52 -0500

    Create phyluceGetFasta.sh

---
+++ phyluceGetFasta.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -J getFasta
+#SBATCH -o getFasta_%A.out
+#SBATCH -e getFasta_%A.err
+#SBATCH -N 1 -n 20
+#SBATCH -t 7-00:00:00
+#SBATCH --mem=30G
+#SBATCH -p short,medium
+#SBATCH -A aphid_phylogenomics
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=yelena.pacheco@usda.gov
+
+module load phyluce/1.7.3
+cd taxon-sets/all
+mkdir log
+
+# run the assembly
+phyluce_assembly_get_fastas_from_match_counts \
+    --contigs ../../spades-assemblies/contigs \
+    --locus-db ../../uce-results/probe.matches.sqlite \
+    --match-count-output all-taxa-incomplete.conf \
+    --output all-taxa-incomplete.fasta \
+    --incomplete-matrix all-taxa-incomplete.incomplete \
+    --log-path log
