a71edb099b65f56fc38f05840c21113bf4386961
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2025-01-16 08:35:11 -0500

    Update phyluce_config.py

--- phyluce_config.py
+++ phyluce_config.py
@@ -7,7 +7,8 @@ import sys
 ##make assembly.conf file for phyluce
 
 with open ('directory.list', 'r') as f:
-	with open ('assembly.conf', 'w') as configFile:
-		for line in f:
-			sampleName=line.split('/',1)[0].strip()
-            configFile.write(sampleName+':/90daydata/leafbeetle_phylogenomics/yelena/Douglas_phyluce/trimmed_reads/'+sampleName+'\n')
+        with open ('assembly.conf', 'w') as configFile:
+                configFile.write('[samples]'+'\n')
+                for line in f:
+                        sampleName=line.strip()
+                        configFile.write(sampleName+':/90daydata/leafbeetle_phylogenomics/yelena/publicDataPhyluce/mergedReads/'+sampleName+'\n')
