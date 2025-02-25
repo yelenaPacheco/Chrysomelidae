ae5730fca2536b6b7729699c439ee5cc69f3ab03
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2025-02-25 09:29:19 -0500

    Update formatRefSeq.py

--- formatRefSeq.py
+++ formatRefSeq.py
@@ -4,15 +4,18 @@ import glob
 import os
 
 
-for file in glob.glob("*.fa"):
-        loci=file.split('.',1)[0].strip()
-        with open (file, 'r+') as f: 
-                with open ('ref_seq.fasta', "a") as newf:
-                        for line in f: 
-                                if line.startswith('>')==True:
-                                        newline=line.strip()+'-'+loci
-                                        print(newline)
-                                        newf.write(newline+'\n')
+for file in glob.glob("*.fa"):								#for all the *.fa files do...
+        loci=file.split('.',1)[0].strip()					#get the name of the files but remove .fa
+        with open (file, 'r+') as f: 						#open the .fa file
+                with open ('ref_seq.fasta', "a") as newf:	#set up to write all sequences in *.fa to a single file
+                        for line in f: 			
+                                if line.startswith('>')==True:				#file line starts with '>' do...
+                                        newline=line.strip()+'-'+loci		#join the current line with the name of .fa file
+                                        print(newline)						#print above output to double check
+                                        newf.write(newline+'\n')			#write to the new file
                                 else:
-                                     	newf.write(line.strip()+'\n')
+                                     	newf.write(line.strip()+'\n')		#if id doesn't start with '>' then write the the new file, this will be your sequence
 
+
+
+#can use to make a reference sequence file from orthofinder output 
