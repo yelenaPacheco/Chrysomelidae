50f3cf3b0b5d9209615729b0b3d60a72fbfddad6
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2025-02-25 09:29:26 -0500

    Create phyluce_edgeTrimming.sh

---
+++ phyluce_edgeTrimming.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J edgeTrim
+#SBATCH -o edgeTrim_%A.out
+#SBATCH -e edgeTrim_%A.err
+#SBATCH -n 20 -N 1
+#SBATCH -p short
+#SBATCH -A aphid_phylogenomics
+#SBATCH -t 2-00:00:00
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=yelena.pacheco@usda.gov
+
+module load phyluce/1.7.3
+
+# align the data
+phyluce_align_seqcap_align \
+    --input all-taxa-incomplete.fasta \
+    --output mafft-nexus-edge-trimmed \
+    --taxa 100 \
+    --aligner mafft \
+    --cores 20\
+    --incomplete-matrix \
+    --log-path log
