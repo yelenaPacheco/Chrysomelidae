acb25b59e50e553e937c5da138fb69561e200342
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2025-02-25 09:29:22 -0500

    Update geneTrees.sh

--- geneTrees.sh
+++ geneTrees.sh
@@ -10,16 +10,20 @@
 #SBATCH --mail-user=yelena.pacheco@usda.gov
 
 
-
+## makes gne trees from alignments, used for coalescent tree
 ##run in trees directory
 
 module load iq_tree/2.2.0
 
-alignments='/90daydata/leafbeetle_phylogenomics/yelena/publicData/concatFasta/AlignFasta/gblocks'
+alignments='/90daydata/leafbeetle_phylogenomics/yelena/publicData/concatFasta/AlignFasta/gblocks'  	#set to directory with all alignment files you want to make trees with, this isn't needed if running directly from alignment dir
 
-infile=$(cat loci2.list | head -n ${SLURM_ARRAY_TASK_ID} | tail -n1)
-#base=$(basename ${infile} _P_R1.fastq)
-
+infile=$(cat loci2.list | head -n ${SLURM_ARRAY_TASK_ID} | tail -n1)		#list of all the alignments 
 
 
-iqtree2 -s ${alignments}/${infile} -m GTR+I+G -bb 1000 -nt 40
+iqtree2 -s ${alignments}/${infile} -m MFP -bb 1000 -nt 40			
+
+## -s input file
+## -m substitution model
+	##  MFP = use model finder 
+## -bb number of bootstrap replicates
+## -nt number of threads, this value should match -n value in job submission above
