a6947469c7d62e266dee819cfd95bbb1c7aa4e4c
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2025-02-25 09:28:43 -0500

    Update convert_TSV_to_FASTA.py

--- convert_TSV_to_FASTA.py
+++ convert_TSV_to_FASTA.py
@@ -3,14 +3,16 @@ from Bio.Seq import Seq
 import glob
 import os
 
-#Converts a .tsv file with format probe number, probe sequence to a FASTA file 
+#Converts a .tsv file with format probe number, probe sequence to a FASTA file, 
+#output file will need further editing for Hybpiper or Phyluce
 
 with open('Coleoptera_AHE_probes.tsv', 'r') as f:
 	with open ('ColeopteraProbes.fasta', 'w') as fasta:
 		for line in f:
 			headerLine = f.readline()
-			locus=line.split("\t")[0].strip()
-			refTaxon=line.split("\t")[1].strip()
-			sequence=line.split("\t")[2].strip()
+			locus=line.split("\t")[0].strip()		#locus number value
+			refTaxon=line.split("\t")[1].strip()	#taxon name
+			sequence=line.split("\t")[2].strip()	#sequence
 			fasta.write(">"+refTaxon+'.'+locus+'\n'+sequence+'\n')
+
 
