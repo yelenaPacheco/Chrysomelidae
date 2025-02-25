17582b03c4e960e2e680152d6e46cb7f6e22821e
yelenaPacheco <70601133+yelenaPacheco@users.noreply.github.com>
2025-02-25 09:29:04 -0500

    Update formatAssemblies.sh

--- formatAssemblies.sh
+++ formatAssemblies.sh
@@ -10,11 +10,16 @@
 #SBATCH --mail-user=yelena.marlese@gmail.com
 
 
+#formats spades assembly directory structure to Phyluce structure
+#spades assemblies were run outside of Phyluce pipeline 
+
 assemblies='/90daydata/leafbeetle_phylogenomics/yelena/publicDataPhyluce/round2_merged/mergedReads/trimmedReadsM/assembliesMerged'
 
-infile=$(cat spadesDir.list | head -n ${SLURM_ARRAY_TASK_ID} | tail -n1)
-base=$(basename ${infile} _spades)
+infile=$(cat spadesDir.list | head -n ${SLURM_ARRAY_TASK_ID} | tail -n1)		#spadesDir.list contains all spades assembly directores
+base=$(basename ${infile} _spades)												#remove _spades from directory name so that you just have species name
 
 
-cp ${infile}/contigs.fasta ${base}.contigs.fasta
-mv ${infile}/${base}.contigs.fasta ${assemblies}
+cp ${infile}/contigs.fasta ${infile}/${base}.contigs.fasta		#copy the contigs.fasta files from species directory to species_spades directory and rename species.contigs.fasta
+mv ${infile}/${base}.contigs.fasta ${assemblies}		#move the species.contigs.fasta to assemblies directory
+														#once contigs are moved to assemblies dir use make_subDir.sh to put in directories with respective species names 
+														#you will still need to make a 'contigs' directory in assembly dir with soft links to species contig files 
